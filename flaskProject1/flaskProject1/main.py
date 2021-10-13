3d61e223660c3551030424ce428ffd0247319e78
AIT00263209student <56629894+AIT00263209student@users.noreply.github.com>
2021-10-13 10:44:27 +0100

    updated project files/sentiment
    - added modules
    - started sentiment analysis portion

--- flaskProject1/flaskProject1/main.py
+++ flaskProject1/flaskProject1/main.py
@@ -1,18 +1,43 @@
+import tweepy
 import tweepy as tp
+import pandas as pd
 from tweepy.auth import AuthHandler
+from textblob import TextBlob
 
-API_KEY = 'NRsqOU5e4r5ZCknOfsdOC2hPt'
-API_SECRET = '2zIqdoVrDSRUc8QD4cG9ScDe4OxxOcMZOCBm0RbGPGpUnUpdhk'
-ACCESS_TOKEN = '1440589525424623629-NQx6rIj1RavoBayKm3qfK63JUPzbjT'
-ACCESS_SECRET = 'IGtvwgOntpd2xSXMui2jmxOqk3pLdvQA9G9LzFEh7XHDnh'
 
-auth = tp.AuthHandler(API_KEY, API_SECRET)
+API_KEY = 'qyfDNcM65yeEp5cnwfDLho1oI'
+API_SECRET = 'nOowF0lDLGdpNPby09FZebPUdBExViNnHf7tw7Fif3MQ68hN3U'
+ACCESS_TOKEN = '1440589525424623629-H19obkwQo8b3KuQc3DPfDTptoeDARB'
+ACCESS_SECRET = 'IcTUsG52XwDA3lG196P13ALJ89IzeSdXempofuSInS7YO'
+
+import tweepy as tp
+
+# authentication
+auth = tp.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 api = tp.API(auth)
 
-tweets = api.search(q='johnny depp')
+# get tweets containing a specific keyword
+tweets = api.search(q='johnny depp', count=10)
+list = []
 
+# print tweets
 for tweet in tweets:
     print(tweet)
-    break
+
+    # sentiment analysis
+    sentiment = TextBlob(tweet.text).sentiment.polarity
+
+    if sentiment > 0.15:
+        sentiment = 'positive'
+    elif sentiment < -0.15:
+        sentiment = 'negative'
+    else:
+        sentiment = 'neutral'
+
+    list.append((tweet.text, sentiment))
+
+df = pd.DataFrame(list)
+
+df.to_csv("output.csv", sep=",") # print to .csv file
---
+++ flaskProject1/flaskProject1/sentiment.py
@@ -0,0 +1,45 @@
+# import SentimentIntensityAnalyzer class
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+# function to print sentence sentiments
+def sentiment_scores(sentence):
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # create sentiment dictionary with polarity scores
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+
+    print("Overall sentiment dictionary is : ", sentiment_dict)
+    print("sentence was rated as ", sentiment_dict['neg'] * 100, "% Negative")
+    print("sentence was rated as ", sentiment_dict['neu'] * 100, "% Neutral")
+    print("sentence was rated as ", sentiment_dict['pos'] * 100, "% Positive")
+
+    print("Sentence Overall Rated As", end=" ")
+
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05:
+        print("Positive")
+
+    elif sentiment_dict['compound'] <= - 0.05:
+        print("Negative")
+
+    else:
+        print("Neutral")
+
+
+# Driver code
+if __name__ == "__main__":
+    print("\n1st statement :")
+    sentence = "i love johnny depp"
+
+    # function calling
+    sentiment_scores(sentence)
+
+    print("\n2nd Statement :")
+    sentence = "watching the new johnny depp movie"
+    sentiment_scores(sentence)
+
+    print("\n3rd Statement :")
+    sentence = "i hate johnny depp"
+    sentiment_scores(sentence)
