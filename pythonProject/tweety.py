ed4830e62135c2c72f6c17624a150b9b1dc40e88
Tara Da Silva <47613037+TaraBells@users.noreply.github.com>
2022-01-02 16:00:02 +0000

    tweet search updates
    updates api keys in tweety.py for amber heard and johnny depp sentiment analysis

--- pythonProject/tweety.py
+++ pythonProject/tweety.py
@@ -3,10 +3,10 @@ from tweepy.auth import AuthHandler
 import pandas as pd
 from textblob import TextBlob
 
-API_KEY = 'pUgp871tte5LCyU33BX9MW04t'
-API_SECRET = 'wd2dzPj4JJ6hAx0XNOryKQFrnDBf8PhPwdRv3PHlz4nkSyF87h'
-ACCESS_TOKEN = '1440589525424623629-hp8Bal0aXxlqLmyo85ZVo0Afg3mYnO'
-ACCESS_SECRET = 'rM6Qayp9m0Mfw0AvdY1kCsYrlahTsBOU1AqyH6VxFyPXg'
+API_KEY = 'qDe68hSUsvmYRqXlb5KAHmIT0'
+API_SECRET = '1tKTQYRYu214pN4AvmJLwWZLgUB5LRsxMJfEu3HvXdaCNs2UuN'
+ACCESS_TOKEN = '1440589525424623629-0M5CfzTaeT28Ek5Sz8pTibZx7pjMVP'
+ACCESS_SECRET = 'HjhakwH3iS2mlrrA9albWmqkRtnq7zNrj0CGlvl0p2xH9'
 
 import tweepy as tp
 
@@ -16,7 +16,7 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tp.API(auth)
 
 tweets = api.search_tweets(q='Johnny Depp', lang="en")
-
+tweets2 = api.search_tweets(q='Amber Heard', lang='en')
 list = []
 
 for tweet in tweets:
@@ -31,6 +31,19 @@ for tweet in tweets:
         sentiment = 'neu'
 
     list.append((tweet.text, sentiment))
+
+    for tweet2 in tweets2:
+
+        sentiment = TextBlob(tweet.text).sentiment.polarity
+
+        if sentiment > 0.15:
+            sentiment = 'positive'
+        elif sentiment < -.15:
+            sentiment = 'neg'
+        else:
+            sentiment = 'neu'
+
+        list.append((tweet2.text, sentiment))
 
 df = pd.DataFrame(list)
 
